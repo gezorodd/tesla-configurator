63e491451dc62d48e4ac6653b6d2f467fe0a7313
gezorodd <geoffrey.zoroddu@zorgi.be>
2024-01-22 18:42:50 +0100

    GitHub Pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,8 @@ import {ApplicationConfig} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
-import {APP_BASE_HREF} from "@angular/common";
+import {provideHttpClient} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), { provide: APP_BASE_HREF, useValue: '/tesla-configurator/' }]
+  providers: [provideRouter(routes), provideHttpClient()]
 };
